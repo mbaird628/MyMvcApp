619b850bb9568e0268f0d5a165aecba430ebb7b9
mark <mark@baird.org>
2018-01-12 19:20:43 -0600

    Adding graphics to ClientValidation.cshtml

--- MyMVCApp/Models/DataValidation.cs
+++ MyMVCApp/Models/DataValidation.cs
@@ -9,31 +9,32 @@ namespace MyMVCApp.Models
 {
     public class DataValidation
     {
-        [Display(Name= "String Length Validation")]
-        [StringLength(5,ErrorMessage ="Need 5 characters.")]
+        [Display(Name= "String Length")]
+        [StringLength(5,ErrorMessage ="Length can be no longer then 5 characters.")]
         public string StringLengthValidation { get; set; }
 
-        [Display(Name ="String range Validation")]
+        [Display(Name ="String range")]
         [StringLength(10,MinimumLength=2)]
         public string StringRangeValidation { get; set; }
 
-        [Display(Name="Required Property Validation")]
+        [Display(Name="Required Property")]
         [Required]
         public string RequiredValidation { get; set; }
 
         // US Phone number in which areas code and the exchange code must start with 2.
+        [Display(Name = "Phone Number")]
         [RegularExpression(@"^\(?([2-9][0-9]{2})\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4})$", ErrorMessage = "Must be of the format: ###-###-#### or (###)###-####. Area and exchange codes must start wtih 2")]
         public string PhoneNumberValidation { get; set; }
 
-        [DisplayName("Email Address Validation")]
+        [DisplayName("Email Address")]
         [EmailAddress]
         public string EmailValidation { get; set; }
 
-        [Display(Name ="Confirm email validation")]
+        [Display(Name ="Confirm email")]
         [Compare("EmailValidation")]
         public string ConfirmationEmailValidation { get; set; }
 
-        [Display(Name="Number Range Validation")]
+        [Display(Name="Number Range")]
         [Range(minimum:4,maximum:10)]
         public int NumberRangeValidation { get; set; }
 
